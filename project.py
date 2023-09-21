4e351a6e7e8bf2d75f00c7c620b15198a59684a6
Ian Zhang <benchi2@pdx.edu>
2023-09-21 12:04:16 -0500

    built a class for extracting the data in the doubly zipped folder

--- project.py
+++ project.py
@@ -7,27 +7,39 @@ from nltk.corpus import stopwords
 import shutil
 from sklearn.feature_extraction.text import CountVectorizer
 
-def extractFile(zipFilePath):
-    with zipfile.ZipFile(zipFilePath, "r") as zipRef:
-        zipRef.extractall("./")
-def moveZipsToRoot(folderName):
-    for fileName in os.listdir(os.path.join("./",folderName)):
-        shutil.move(os.path.join("./"+folderName, fileName), os.path.join("./", fileName))
-
+class dataExtracter:
+    def __init__(self, zipName):
+        self.allData = {"spam": [], "ham": []}
+        self.spamData = []
+        self.hamData = []
+        self.folderName = zipName.lower()
+        self.zipPath = os.path.join("./", self.folderName+".zip")
+    def extractFile():
+        with zipfile.ZipFile(self.zipPath, "r") as folder:
+            folder.extractall("./")
+    def moveZipToRoot():
+        for fileName in os.listdir(os.path.join("./",self.folderName)):
+            shutil.move(os.path.join("./" + self.folderName, fileName), os.path.join("./", fileName))
+    def readData(dataSetName, trainOrTest, spamOrHam):
+        with zipfile.ZipFile(os.path.join("./", dataSetName.lower()+"_"+trainOrTest.lower()+".zip"), "r") as zipRef:
+            fils = [f for f in zipRef.namelist() if f.endswith(spamOrHam.lower()+".txt")]
+            for file in files:
+                self.allData[spamOrHam.lower()].append(io.TextIOWrapper(zipRef.open(file), encoding='iso-8859-1', newline='').read())
+        
 # extractFile("./project1_datasets.zip")
 # moveZipsToRoot("project1_datasets")
 
 # with zipfile.ZipFile("./project1_datasets.zip", "r") as zipRef:
 #     print(zipRef.namelist())
-spamData = []
-hamData = []
-with zipfile.ZipFile("./enron1_train.zip", "r") as zipRef:
-    spamFiles = [f for f in zipRef.namelist() if f.endswith("spam.txt")]
-    hamFiles = [f for f in zipRef.namelist() if f.endswith("ham.txt")]
-    for file in spamFiles:
-        spamData.append(io.TextIOWrapper(zipRef.open(file), encoding='iso-8859-1', newline='').read())
-    for file in hamFiles:
-        hamData.append(io.TextIOWrapper(zipRef.open(file), encoding='iso-8859-1', newline='').read())
+# spamData = []
+# hamData = []
+# with zipfile.ZipFile("./enron1_train.zip", "r") as zipRef:
+#     spamFiles = [f for f in zipRef.namelist() if f.endswith("spam.txt")]
+#     hamFiles = [f for f in zipRef.namelist() if f.endswith("ham.txt")]
+#     for file in spamFiles:
+#         spamData.append(io.TextIOWrapper(zipRef.open(file), encoding='iso-8859-1', newline='').read())
+#     for file in hamFiles:
+#         hamData.append(io.TextIOWrapper(zipRef.open(file), encoding='iso-8859-1', newline='').read())
     
     # for innerFile in zipFiles:
     #     innerFileData = zipRef.read(innerFile)
