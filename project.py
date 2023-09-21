091adacb9442e12c0f0a8062e76096c8fe850e1e
Ian Zhang <benchi2@pdx.edu>
2023-09-20 19:49:02 -0500

    import libraries

---
+++ project.py
@@ -0,0 +1,7 @@
+import zipfile
+import os
+import numpy as np 
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+
