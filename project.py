766e83fd136baef335483aeda3dd6765a4cf162d
Ian Zhang <benchi2@pdx.edu>
2023-09-22 14:20:55 -0500

    fixed a bug in the dataExtractor class

--- project.py
+++ project.py
@@ -9,7 +9,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 class dataExtracter:
     def __init__(self, zipName):
-        self.allData = {"spam": [], "ham": []}
+        self.allData = {"train": {"spam": [], "ham": []}, "test": {"spam": [], "ham": []}}
         self.folderName = zipName.lower()
         self.zipPath = os.path.join("./", self.folderName+".zip")
         self._extractFile()
@@ -20,17 +20,18 @@ class dataExtracter:
     def _moveZipToRoot(self):
         for fileName in os.listdir(os.path.join("./",self.folderName)):
             shutil.move(os.path.join("./" + self.folderName, fileName), os.path.join("./", fileName))
-    def readData(self,dataSetName, trainOrTest, spamOrHam):
-        with zipfile.ZipFile(os.path.join("./", dataSetName.lower()+"_"+trainOrTest.lower()+".zip"), "r") as zipRef:
-            files = [f for f in zipRef.namelist() if f.endswith(spamOrHam.lower()+".txt")]
-            for file in files:
-                self.allData[spamOrHam.lower()].append(io.TextIOWrapper(zipRef.open(file), encoding='iso-8859-1', newline='').read())
+    def readData(self,dataSetName, trainOrTest):
+        for trainOrTest in ["train", "test"]:
+            with zipfile.ZipFile(os.path.join("./", dataSetName.lower()+"_"+trainOrTest.lower()+".zip"), "r") as zipRef:
+                for spamOrHam in ["spam", "ham"]:
+                    files = [f for f in zipRef.namelist() if f.endswith(spamOrHam.lower()+".txt")]
+                    for file in files:
+                        self.allData[trainOrTest][spamOrHam.lower()].append(io.TextIOWrapper(zipRef.open(file), encoding='iso-8859-1', newline='').read())
 
 extracted = dataExtracter("project1_datasets")
-extracted.readData("enron1", "train", "spam")
-extracted.readData("enron1", "train", "ham")
-print(extracted.allData["spam"][0])
-print(extracted.allData["ham"][0])
+extracted.readData("enron1")
+print(extracted.allData["train"]["spam"][0])
+print(extracted.allData["train"]["ham"][0])
 
 class spamClassifier:
     def __init__(self):
@@ -38,12 +39,8 @@ class spamClassifier:
         self.bagOfWordsFeatures = {"train": None, "test": None}
         self.bernoulli = {"train":None, "test":None}
         self.bernoulliFeatures = {"train":None, "test":None}
-        # self.dataSetName = dataSetName
-        # self.trainData = []
-        # self.testData = []
     def _tokenizeText(self,text):
         tokens = word_tokenize(text)
-        # self.words = None
         tokens = [word.lower() for word in tokens if word.isalnum()]
         stopWords = set(stopwords.words('english'))
         tokens = [word for word in tokens if word not in stopWords]
@@ -67,7 +64,7 @@ class spamClassifier:
     def createRepresentations(self, rawText, trainOrTest):
         tokenizedData, Y = self._processData(rawText)
         self._bagOfWords(tokenizedData, Y, trainOrTest)
-        self.bernoulli(tokenizedData, Y, trainOrTest)
+        self._bernoulli(tokenizedData, Y, trainOrTest)
         
 
         
